479138a8bcb3763ec7334775c5ba1cc5124e6474
Suhail-Ahmed7 <suhailbhand7@gmail.com>
2025-06-09 01:26:57 +0500

    Index file added!

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/dbConnection');
+dotenv.config();
+connectDB();
+
+const PORT = process.env.PORT || 8080;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
