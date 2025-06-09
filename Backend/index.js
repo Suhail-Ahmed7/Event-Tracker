ebd7b0dacef0374374d8579e3ba0db45e1566caa
Suhail-Ahmed7 <suhailbhand7@gmail.com>
2025-06-09 13:32:49 +0500

    Modify index file for checking routes

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
+const AuthRouter = require('./routes/authRouter');
+
 const connectDB = require('./config/dbConnection');
 dotenv.config();
 connectDB();
@@ -11,6 +13,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
+app.use('/auth', AuthRouter);
 
 
 app.listen(PORT, () => {
