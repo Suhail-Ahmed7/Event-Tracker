dacecad1d6154efed5e67ce31194eff2ec0a36ef
Suhail-Ahmed7 <suhailbhand7@gmail.com>
2025-06-10 22:04:56 +0500

    Ad

--- Backend/controllers/eventController.js
+++ Backend/controllers/eventController.js
@@ -17,3 +17,75 @@ const createEvent = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+const getAllEvents = async (req, res) => {
+    try {
+        const events = await Event.find()
+        res.status(200).json(events);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+
+}
+
+const getEventById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const event = await Event.findById(id);
+
+        if (!event) {
+            return res.status(404).json({ message: 'Event not found' });
+        }
+
+        res.status(200).json(event);
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+
+const updateEventById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, description, date, location } = req.body;
+
+        const event = await Event.findByIdAndUpdate(
+            id,
+            { title, description, date, location },
+            { new: true } 
+        );
+
+        if (!event) {
+            return res.status(404).json({ message: 'Event not found' });
+        }
+
+        res.status(200).json(event);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+const deleteEventById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const event = await Event.findByIdAndDelete(id);
+
+        if (!event) {
+            return res.status(404).json({ message: 'Event not found' });
+        }
+
+        res.status(200).json({ message: 'Event deleted successfully' });
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+
+module.exports = {
+    createEvent,
+    getAllEvents,
+    getEventById,
+    updateEventById,
+    deleteEventById
+}
