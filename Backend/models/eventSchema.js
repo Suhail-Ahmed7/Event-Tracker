9def210f6d0196369b2eedc157e92e7b7e6508b6
Suhail-Ahmed7 <suhailbhand7@gmail.com>
2025-06-10 17:03:39 +0500

    Event Schema added

---
+++ Backend/models/eventSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const eventSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required : true
+    }, 
+    description : {
+        type : String,
+        required : true,
+    },
+    date : {
+        type : Number,
+        required : true
+    },
+    location: {
+        type: String,
+        required : true
+    }
+
+})
+
+module.exports = mongoose.model('events', eventSchema)
