753e3a49d45a40ae4d36b37b0cb0dac0868763c2
Suhail-Ahmed7 <suhailbhand7@gmail.com>
2025-06-13 11:34:59 +0500

    Insert user id to get event

--- Backend/controllers/eventController.js
+++ Backend/controllers/eventController.js
@@ -1,63 +1,61 @@
-const Event = require('../models/eventSchema')
+const Event = require('../models/eventSchema');
 
 const createEvent = async (req, res) => {
     try {
         const { title, description, date, location } = req.body;
+        const userId = req.user._id;
 
         const event = await Event.create({
             title,
             description,
             date,
-            location
+            location,
+            userId
         });
 
         res.status(201).json({ message: "Event created successfully", event });
-
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 };
+
 const getAllEvents = async (req, res) => {
     try {
-        const events = await Event.find()
+        const events = await Event.find({ userId: req.user._id });
         res.status(200).json(events);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
-
-}
+};
 
 const getEventById = async (req, res) => {
     try {
         const { id } = req.params;
-
-        const event = await Event.findById(id);
+        const event = await Event.findOne({ _id: id, userId: req.user._id });
 
         if (!event) {
-            return res.status(404).json({ message: 'Event not found' });
+            return res.status(404).json({ message: 'Event not found or not authorized' });
         }
 
         res.status(200).json(event);
-
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 };
-
 
 const updateEventById = async (req, res) => {
     try {
         const { id } = req.params;
         const { title, description, date, location } = req.body;
 
-        const event = await Event.findByIdAndUpdate(
-            id,
+        const event = await Event.findOneAndUpdate(
+            { _id: id, userId: req.user._id },
             { title, description, date, location },
-            { new: true } 
+            { new: true }
         );
 
         if (!event) {
-            return res.status(404).json({ message: 'Event not found' });
+            return res.status(404).json({ message: 'Event not found or not authorized' });
         }
 
         res.status(200).json(event);
@@ -65,23 +63,21 @@ const updateEventById = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
 const deleteEventById = async (req, res) => {
     try {
         const { id } = req.params;
-        const event = await Event.findByIdAndDelete(id);
+        const event = await Event.findOneAndDelete({ _id: id, userId: req.user._id });
 
         if (!event) {
-            return res.status(404).json({ message: 'Event not found' });
+            return res.status(404).json({ message: 'Event not found or not authorized' });
         }
 
         res.status(200).json({ message: 'Event deleted successfully' });
-
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 };
-
-
 
 module.exports = {
     createEvent,
@@ -89,4 +85,4 @@ module.exports = {
     getEventById,
     updateEventById,
     deleteEventById
-}
+};
